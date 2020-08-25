0b87a7595e261db95dcdacdf25951d6f47984edf
yuji okayama <yujiokayama@yujinoMacBook-Pro.local>
2020-08-25 23:19:36 +0900

    検索窓構築途中

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,58 +1,91 @@
-import React, { Fragment, useContext, useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 
-import { Button, Container, Grid, Typography } from "@material-ui/core";
+import {
+  Button,
+  Container,
+  Grid,
+  Typography,
+  Paper,
+  IconButton,
+  InputBase,
+} from "@material-ui/core";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
+import SearchIcon from "@material-ui/icons/Search";
+
+import pencil from "../assets/images/pencil.jpg";
 
 import { AuthContext } from "../Auth";
-import firebase from "firebase";
 import auth from "../firebase";
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    background: {
+      backgroundImage: `url(${pencil})`,
+      backgroundSize: "cover",
+      height: "100vh",
+    },
+    paper: {
+      position: "relative",
+      marginRight: "auto",
+      marginLeft: "auto",
+      top: "33%",
+      width: "45%",
+    },
+  })
+);
 
 const Home: React.FC = (props: any) => {
   const { currentUser } = useContext(AuthContext);
+  const classes = useStyles();
 
   useEffect(() => {
-    // if not logged in, redirect to login page
     currentUser === null && props.history.push("/login");
   }, [currentUser]);
 
   return (
-    <Fragment>
-      <Container>
-        <Grid container style={{ marginTop: "1em" }}>
-          <Grid item md={4}></Grid>
-          <Grid item md={4}>
-            <Typography
-              variant="caption"
-              style={{
-                paddingTop: "2em",
-                paddingBottom: "2em",
-                whiteSpace: "pre",
-              }}
-            >
-              {currentUser && JSON.stringify(currentUser, null, 4)}
-            </Typography>
-            <h1>
-              Hello: {currentUser && currentUser.displayName}!!
-            </h1>
+    <>
+      <div className={classes.background}>
+        <Paper className={classes.paper}>
+          <IconButton type="submit">
+            <SearchIcon />
+          </IconButton>
+          <InputBase placeholder="検索ワードを入力" />
+        </Paper>
+        <Container>
+          <Grid container style={{ marginTop: "1em" }}>
+            <Grid item md={4}></Grid>
+            <Grid item md={4}>
+              <Typography
+                variant="caption"
+                style={{
+                  paddingTop: "2em",
+                  paddingBottom: "2em",
+                  whiteSpace: "pre",
+                }}
+              >
+                {/* {currentUser && JSON.stringify(currentUser, null, 4)} */}
+              </Typography>
+              <h1>Hello: {currentUser && currentUser.displayName}!!</h1>
 
-            <Button
-              fullWidth
-              onClick={async (e) => {
-                try {
-                  await auth.signOut();
-                  props.history.push("/login");
-                } catch (error) {
-                  alert(error.message);
-                }
-              }}
-              style={{ marginTop: "0.5em", marginBottom: "0.5em" }}
-            >
-              Logout
-            </Button>
+              <Button
+                fullWidth
+                onClick={async (e) => {
+                  try {
+                    await auth.signOut();
+                    props.history.push("/login");
+                  } catch (error) {
+                    alert(error.message);
+                  }
+                }}
+                style={{ marginTop: "0.5em", marginBottom: "0.5em" }}
+              >
+                Logout
+              </Button>
+            </Grid>
           </Grid>
-          <Grid item md={4}></Grid>
-        </Grid>
-      </Container>
-    </Fragment>
+        </Container>
+      </div>
+    </>
   );
 };
 
